f2ab36ac8f35f37e885228ef7ea5988143679f64
Enrique <hsandovals@uni.pe>
2025-04-30 12:16:17 -0500

    correcion en earth_engine

--- utils/earth_engine.py
+++ utils/earth_engine.py
@@ -4,17 +4,21 @@ import json
 
 
 def init_earth_engine():
-    creds_json = os.getenv("GEE_CREDENTIALS_JSON")  # Asegúrate que esté bien cargada
+    creds_json = os.getenv("GEE_CREDENTIALS_JSON")
     if not creds_json:
         raise ValueError("No se encontró la variable de entorno GEE_CREDENTIALS_JSON")
 
-    # Guardar temporalmente en archivo local
-    with open("service_account.json", "w") as f:
+    # Guardar las credenciales como archivo temporal
+    cred_path = "service_account.json"
+    with open(cred_path, "w") as f:
         f.write(creds_json)
 
-    # Inicializar Earth Engine con archivo
-    service_account = json.loads(creds_json)["client_email"]
-    credentials = ee.ServiceAccountCredentials(service_account, "service_account.json")
+    # Leer el email desde el archivo ya guardado
+    with open(cred_path) as f:
+        creds_dict = json.load(f)
+
+    service_account = creds_dict["client_email"]
+    credentials = ee.ServiceAccountCredentials(service_account, cred_path)
     ee.Initialize(credentials)
     
 '''
