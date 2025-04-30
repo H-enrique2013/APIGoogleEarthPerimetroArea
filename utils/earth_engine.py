ac6aeafdda127660f7bebfe61316d98f2e3d59b0
Enrique <hsandovals@uni.pe>
2025-04-30 12:30:18 -0500

    error credenciales

--- utils/earth_engine.py
+++ utils/earth_engine.py
@@ -9,13 +9,11 @@ def init_earth_engine():
         raise ValueError("No se encontró la variable de entorno GEE_CREDENTIALS_JSON")
 
     try:
-        # Desescapar texto si viene con \\n
-        creds_unescaped = bytes(creds_json, "utf-8").decode("unicode_escape")
-        creds_dict = json.loads(creds_unescaped)
-    except Exception as e:
-        raise ValueError(f"Error procesando GEE_CREDENTIALS_JSON: {e}")
+        # Esto es suficiente si ya pegaste el JSON con comillas escapadas desde Render
+        creds_dict = json.loads(creds_json)
+    except json.JSONDecodeError as e:
+        raise ValueError(f"Error decodificando el JSON de GEE_CREDENTIALS_JSON: {e}")
 
-    # Guardar archivo temporal para autenticar
     cred_path = "service_account.json"
     with open(cred_path, "w") as f:
         json.dump(creds_dict, f)
@@ -23,6 +21,9 @@ def init_earth_engine():
     service_account = creds_dict["client_email"]
     credentials = ee.ServiceAccountCredentials(service_account, cred_path)
     ee.Initialize(credentials)
+
+    # Opcional: borrar el archivo
+    # os.remove(cred_path)
     
 '''
 def init_earth_engine():
