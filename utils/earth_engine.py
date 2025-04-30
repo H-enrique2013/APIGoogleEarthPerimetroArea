8ede153c375af7561fbd967c3724ba5e7b877651
Enrique <hsandovals@uni.pe>
2025-04-29 21:28:34 -0500

    cambios en credenciales

--- utils/earth_engine.py
+++ utils/earth_engine.py
@@ -4,21 +4,19 @@ import json
 
 
 def init_earth_engine():
-    creds_json = os.environ.get("CREDENTIALS_JSON")
+    creds_json = os.getenv("GEE_CREDENTIALS_JSON")  # Asegúrate que esté bien cargada
     if not creds_json:
-        raise Exception("Variable de entorno 'CREDENTIALS_JSON' no definida")
+        raise ValueError("No se encontró la variable de entorno GEE_CREDENTIALS_JSON")
 
-    # Guardar temporalmente el contenido para inicializar EE
-    with open("/tmp/credentials.json", "w") as f:
+    # Guardar temporalmente en archivo local
+    with open("service_account.json", "w") as f:
         f.write(creds_json)
 
-    creds_dict = json.loads(creds_json)
-    credentials = ee.ServiceAccountCredentials(
-        email=creds_dict["client_email"],
-        key_file="/tmp/credentials.json"
-    )
+    # Inicializar Earth Engine con archivo
+    service_account = json.loads(creds_json)["client_email"]
+    credentials = ee.ServiceAccountCredentials(service_account, "service_account.json")
     ee.Initialize(credentials)
- 
+    
 '''
 def init_earth_engine():
     credentials = ee.ServiceAccountCredentials(
