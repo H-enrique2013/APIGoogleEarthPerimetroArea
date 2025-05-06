01fbb2869b4f593fd42e1c41b9474f949f206d25
Enrique <hsandovals@uni.pe>
2025-05-06 11:01:13 -0500

    Cambios en GetMapid

--- main.py
+++ main.py
@@ -17,23 +17,96 @@ def hello():
 def get_map_id():
     try:
         data = request.get_json()
+        satellite = data.get('satellite')
         start = data.get('startDate')
         end = data.get('endDate')
         index = data.get('index')
 
-        collection = ee.ImageCollection("COPERNICUS/S2_SR").filterDate(start, end).median()
+        if not all([start, end, index, satellite]):
+            return jsonify({'error': 'Faltan parámetros requeridos'}), 400
 
-        if index == 'NDVI':
-            ndvi = collection.normalizedDifference(['B8', 'B4']).rename('NDVI')
-            vis_params = {'min': 0, 'max': 1, 'palette': ['white', 'green']}
-            image = ndvi.visualize(**vis_params)
+        # Obtener la colección según el satélite
+        if satellite == 'Sentinel-2':
+            collection = ee.ImageCollection("COPERNICUS/S2_SR").filterDate(start, end).median()
+
+            if index == 'NDVI':
+                image = collection.normalizedDifference(['B8', 'B4']).rename('NDVI')
+                vis_params = {'min': -1, 'max': 1, 'palette': ['blue', 'white', 'green']}
+                image = image.visualize(**vis_params)
+
+            elif index == 'NDWI':
+                image = collection.normalizedDifference(['B3', 'B8']).rename('NDWI')
+                vis_params = {'min': -1, 'max': 1, 'palette': ['white', 'blue']}
+                image = image.visualize(**vis_params)
+
+            elif index == 'NDBI':
+                image = collection.normalizedDifference(['B11', 'B8']).rename('NDBI')
+                vis_params = {'min': -1, 'max': 1, 'palette': ['white', 'brown']}
+                image = image.visualize(**vis_params)
+
+            elif index == 'RGB':
+                vis_params = {'min': 0, 'max': 3000, 'bands': ['B4', 'B3', 'B2']}
+                image = collection.visualize(**vis_params)
+
+            else:
+                return jsonify({'error': f'Índice "{index}" no soportado para Sentinel-2'}), 400
+
+        elif satellite == 'Landsat-8':
+            collection = ee.ImageCollection("LANDSAT/LC08/C02/T1_L2").filterDate(start, end).median()
+
+            if index == 'NDVI':
+                image = collection.normalizedDifference(['SR_B5', 'SR_B4']).rename('NDVI')
+                vis_params = {'min': -1, 'max': 1, 'palette': ['blue', 'white', 'green']}
+                image = image.visualize(**vis_params)
+
+            elif index == 'NDWI':
+                image = collection.normalizedDifference(['SR_B3', 'SR_B5']).rename('NDWI')
+                vis_params = {'min': -1, 'max': 1, 'palette': ['white', 'blue']}
+                image = image.visualize(**vis_params)
+
+            elif index == 'NDBI':
+                image = collection.normalizedDifference(['SR_B6', 'SR_B5']).rename('NDBI')
+                vis_params = {'min': -1, 'max': 1, 'palette': ['white', 'brown']}
+                image = image.visualize(**vis_params)
+
+            elif index == 'RGB':
+                vis_params = {'min': 0, 'max': 30000, 'bands': ['SR_B4', 'SR_B3', 'SR_B2']}
+                image = collection.visualize(**vis_params)
+
+            else:
+                return jsonify({'error': f'Índice "{index}" no soportado para Landsat-8'}), 400
+
+        elif satellite == 'Landsat-9':
+            collection = ee.ImageCollection("LANDSAT/LC09/C02/T1_L2").filterDate(start, end).median()
+
+            if index == 'NDVI':
+                image = collection.normalizedDifference(['SR_B5', 'SR_B4']).rename('NDVI')
+                vis_params = {'min': -1, 'max': 1, 'palette': ['blue', 'white', 'green']}
+                image = image.visualize(**vis_params)
+
+            elif index == 'NDWI':
+                image = collection.normalizedDifference(['SR_B3', 'SR_B5']).rename('NDWI')
+                vis_params = {'min': -1, 'max': 1, 'palette': ['white', 'blue']}
+                image = image.visualize(**vis_params)
+
+            elif index == 'NDBI':
+                image = collection.normalizedDifference(['SR_B6', 'SR_B5']).rename('NDBI')
+                vis_params = {'min': -1, 'max': 1, 'palette': ['white', 'brown']}
+                image = image.visualize(**vis_params)
+
+            elif index == 'RGB':
+                vis_params = {'min': 0, 'max': 30000, 'bands': ['SR_B4', 'SR_B3', 'SR_B2']}
+                image = collection.visualize(**vis_params)
+
+            else:
+                return jsonify({'error': f'Índice "{index}" no soportado para Landsat-9'}), 400
+
         else:
-            vis_params = {'min': 0, 'max': 3000, 'bands': ['B4', 'B3', 'B2']}
-            image = collection.visualize(**vis_params)
+            return jsonify({'error': 'Satélite no soportado'}), 400
 
+        # Obtener map ID
         map_id = ee.data.getMapId({'image': image})
 
-        # Extraer solo los datos serializables
         return jsonify({
             'mapid': map_id['mapid'],
             'token': map_id['token']
@@ -41,8 +114,6 @@ def get_map_id():
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-
 
 @app.route('/CalculoAreaNDVIPerimetro', methods=['POST'])
 def calcular_ndvi_area_perimetro():
